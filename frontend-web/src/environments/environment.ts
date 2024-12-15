08f6fe72ef343f05705e4df31d6aa91f4d41b825
thibe <thibeau.nauwelaerts@student.pxl.be>
2024-12-15 15:33:05 +0100

    environment.ts toegevoegd

---
+++ frontend-web/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    postUrl: 'http://localhost:8083/post/posts',
+    reviewApiUrl: 'http://localhost:8083/review/reviews',
+    commentApiUrl: 'http://localhost:8083/comment/comments'
+  };
