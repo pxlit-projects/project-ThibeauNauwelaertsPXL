5fbf70d20a6530947129f59a5e149e42a46f860a
thibe <thibeau.nauwelaerts@student.pxl.be>
2025-01-09 22:50:33 +0100

    Probleem is opgelost

--- frontend-web/src/main.ts
+++ frontend-web/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient(), provideRouter(routes)],
